0870b341071248a39ca21530f1970798ce44962b
Sergio Vieri <sergio.vieri.hp@gmail.com>
2021-05-15 11:59:57 +0800

    Update default net to nn-60c78a44f957.nnue
    512x2->16->32->1
    
    Change maxply to 80, count to 10.
    
    Drop LR from 0.2 to 0.1.
    
    Bench: 4039175

--- src/evaluate.h
+++ src/evaluate.h
@@ -39,7 +39,7 @@ namespace Eval {
   // The default net name MUST follow the format nn-[SHA256 first 12 digits].nnue
   // for the build process (profile-build and fishtest) to work. Do not change the
   // name of the macro, as it is used in the Makefile.
-  #define EvalFileDefaultName   "nn-3817df118961.nnue"
+  #define EvalFileDefaultName   "nn-60c78a44f957.nnue"
 
   namespace NNUE {
 
